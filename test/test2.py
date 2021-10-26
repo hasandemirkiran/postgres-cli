9dd40e8340ad11744a7748f27d3f90481f2a7f23
hasan-OCELL <hasan.demirkiran@ocell.io>
2021-10-26 14:54:53 +0200

    test

--- test/test2.py
+++ test/test2.py
@@ -1,22 +1,7 @@
-import pandas as pd
-import geopandas as gpd
-from shapely.geometry import Polygon, Point, box
-from shapely.geometry.linestring import LineString
+import sqlalchemy as db
 
-# print(type([12.542811636363634, 48.50293363636363, 12.553058, 48.51191581818186]))
-# print(type(box(12.542811636363634, 48.50293363636363, 12.553058, 48.51191581818186)))
-
-
-def normalize_bbox(bbox):
-    def swap(i_a, i_b):
-        tmp = bbox[i_a]
-        bbox[i_a] = bbox[i_b]
-        bbox[i_b] = tmp
-    def fix_order(i_a, i_b):
-        if bbox[i_a] > bbox[i_b]:
-            swap(i_a, i_b)
-    fix_order(0, 2)
-    fix_order(1, 3)
-    return bbox
-
-print(normalize_bbox([12.553058,48.50293363636363,  48.51191581818186, 12.542811636363634,]))
+engine = db.create_engine('postgresql://ocell:PdUfpcWSYh4y3Cg@labeldb.cxitxpc33tur.eu-central-1.rds.amazonaws.com:5432/hasan_test')
+connection = engine.connect()
+metadata = db.MetaData()
+raster_info = db.Table('raster_info', metadata, autoload=True, autoload_with=engine)
+query = db.select([raster_info]).where(raster_info.columns.sex == 'F')
