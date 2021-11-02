f9c4d68b673960bdac47ad509fbcbc20f2a32c8d
hasan-OCELL <hasan.demirkiran@ocell.io>
2021-11-02 10:48:03 +0100

    Update new database credentials

--- Helper/fetch_data_with_queries.py
+++ Helper/fetch_data_with_queries.py
@@ -1,11 +1,11 @@
 import sqlalchemy as db
 import pprint
 
-engine = db.create_engine('postgresql://ocell:PdUfpcWSYh4y3Cg@labeldb.cxitxpc33tur.eu-central-1.rds.amazonaws.com:5432/hasan_test')
+engine = db.create_engine('postgresql://postgres:PdUfpcWSYh4y3Cg@geodb.c6pejgcymcj0.eu-central-1.rds.amazonaws.com:5432/postgres')
 connection = engine.connect()
 metadata = db.MetaData()
 raster_info = db.Table('raster_info', metadata, autoload=True, autoload_with=engine)
-query = db.select([raster_info]).where(raster_info.columns.name == 'Dist_12_13')
+query = db.select([raster_info]).where(raster_info.columns.name == 'Magdeburg')
 ResultProxy = connection.execute(query)
 ResultSet = ResultProxy.fetchall()
 pprint.pprint(ResultSet)
