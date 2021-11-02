5834e14b1f286be681806d624af26c90d39b48e0
hasan-OCELL <hasan.demirkiran@ocell.io>
2021-11-02 10:47:05 +0100

    Helper Script to compare versions for label files

---
+++ Helper/compare_versions.py
@@ -0,0 +1,49 @@
+import json
+import geojson    
+import numpy as np   
+
+# compare 2 files if the label areas overlap or not 
+def compare_2_files(URL_1, URL_2):
+    tile_list_1 = []
+    tile_list_2 = []
+
+    with open(URL_1) as json_file:
+        data = json.load(json_file)
+        for feature_collection in data:
+            temp_tile = feature_collection['properties']['tile']
+            tile_list_1.append(temp_tile)
+
+    with open(URL_2) as json_file:
+        data = json.load(json_file)
+        for feature_collection in data:
+            temp_tile = feature_collection['properties']['tile']
+            tile_list_2.append(temp_tile)
+
+    counter_1 = 0
+    for tile in tile_list_1:
+        if tile not in tile_list_2:
+            counter_1 += 1 
+
+    counter_2 = 0
+    for tile in tile_list_2:
+        if tile not in tile_list_1:
+            counter_2 += 1 
+    
+    if counter_1 == 0 and counter_2 == 0:
+        print ('They are the same files.')
+    elif counter_1 != 0 and counter_2 == 0 :
+        print ('First file is the more inclusive.')
+    elif counter_2 != 0 and counter_1 == 0:
+        print ('Second file is the more inclusive.')
+    else:
+        if counter_1 == len(tile_list_1) and counter_2 == len(tile_list_2):
+            print ('They are completely different files.')
+        else:
+            print ('They are different files but there is an intersection.')  
+
+
+
+if __name__ == "__main__":
+    URL_1 = '\\\\192.168.37.4\\ml\\datasets\\forestry\\CenterForst_Magdeburg\\class_label_v1.json'
+    URL_2 = '\\\\192.168.37.4\\ml\\datasets\\forestry\\CenterForst_Magdeburg\\center_label_v1.json'
+    compare_2_files(URL_1,URL_2)
