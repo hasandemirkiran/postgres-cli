f93a649838388faedda435398985dba1559601e5
luca <luca.martinatti@gmail.com>
2021-11-24 17:58:58 +0100

    Add query section

---
+++ postgres_cli/query_tools/automatic_query.py
@@ -0,0 +1,277 @@
+import os
+import argparse
+import psycopg2
+import pandas as pd
+import datetime
+from configparser import ConfigParser, Error
+from geojson import Point, Feature, FeatureCollection, dump, Polygon
+import json
+import yaml
+from copy import deepcopy
+
+from configs.classification_problem_config import base_config
+import utils.queries_file
+
+
+def db_config(filename, section='postgresql'):
+    """Load configurations for database connection
+
+    Args:
+        filename (str): Path of the credentials and configurations (.ini) file necessary to connect at the database.
+        section (str): Analyzed section of the configuration (.ini) file. Defaults to 'postgresql'.
+
+    Raises:
+        Exception: If the required section is not present in the configuration file
+
+    Returns:
+        dict: Configuration data
+    """
+
+    # create a parser
+    parser = ConfigParser()
+    
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+
+
+def fetch_from_label_db(query):
+    """Execute the query to fetch the data from the label database.
+
+    Args:
+        query (str): Query for the label database.
+
+    Returns:
+        pandas.DataFrame: DataFrame containing the data fetched according the query.
+    """
+
+    try:
+        params = db_config(filename='database.ini', section='postgresql')
+
+        # Connect to database
+        connection = psycopg2.connect(**params)
+        #print("--- Label database connection was oppened ---")
+        cursor = connection.cursor()
+
+        # Execute the query
+        fetched_data = pd.io.sql.read_sql(query, connection)
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+        return None
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+            #print("--- Label database connection was closed ---")
+        return fetched_data
+
+def update_config(config, label_path, raster_path):
+    config["make_label_mappings"] = {"label_path": label_path}
+    config["make_dataset__list"].append(
+            {
+                "image_path": raster_path,
+                "label_path": label_path,
+                "splits": 0,
+            }
+    )
+
+def create_label_file(features, out_folder, region_name):
+    feature_collection = FeatureCollection(features)
+    save_path =  os.path.join(out_folder, region_name)
+    with open('{}.geojson'.format(save_path), 'w') as f:
+        dump(feature_collection, f)
+
+if __name__ == "__main__":
+
+    
+    parser = argparse.ArgumentParser(description="""Query label data""")
+    parser.add_argument("--query_type", choices=['similar_conditions', 'temporal_intrval', 'single_tree_percentage', 'region_name'], type=str, help="Type of query used for the label database.")
+    
+    # single_tree_percentage parameters
+    parser.add_argument("--reference_tree", type=str, help="Class of tree to use as reference one")
+    parser.add_argument("--max_percentage", type=int, default=100, help="Maximum percentage of the specific tree present in the bbox")
+    parser.add_argument("--min_percentage", type=int, default=0, help="Minimum percentage of the specific tree present in the bbox")
+    
+    # region_name parameters
+    parser.add_argument("--region", type=str, help="Name of the region to analyze")
+
+    # similar_season parameters
+    parser.add_argument("--date", type=datetime.date.fromisoformat, help="Starting date for the temporal search. Input format YYYY-MM-DD")
+    parser.add_argument("--days", default=10 ,type=int, help="Interval of days to analyze before and after the starting date. Input format YYYY-MM-DD")
+    parser.add_argument("--months", default=0, type=int, help="Interval of months to analyze before and after the starting date. Input format YYYY-MM-DD")
+
+    #parser.add_argument("--max_cloud_presence", type=float, help="Maximum level of cloud presence in the scene")
+    #parser.add_argument("--min_cloud_presence", type=float, help="Minimum level of cloud presence in the scene")
+
+    #parser.add_argument("--starting_date", type=datetime.date.fromisoformat, help="Starting date for the temporal search. Input format YYYY-MM-DD hh:mm")
+
+    parser.add_argument("--output_path", type=str, help="Path for the output file")
+
+    args = parser.parse_args()
+
+    try:
+        # Fetch data
+        df = fetch_from_label_db('SELECT id AS id, name AS name FROM public.tree_class;')
+        tree_types = df.set_index('name').to_dict()['id']
+    except Exception as error:
+        print("Error in the tree classes data fetching", error)
+        quit()
+
+    # Create output folder
+    now = datetime.datetime.now() 
+    current_time = now.strftime("%Y_%m_%d-%H_%M_%S")
+    out_folder = os.path.join(args.output_path, 'training_data_{}/'.format(current_time))
+    os.mkdir(out_folder)
+
+    ### Prepare the config file
+    config = deepcopy(base_config)
+    config["Albunet"]["dropout"] = 0.0
+    config["make_loader"]["batch_size"] = 8
+    config["do_training"]["job_name"] = "classification"
+    config["do_training"]["epochs"] = 2000
+    config["make_dataset__list"] = []
+
+    try:
+        if args.query_type == 'single_tree_percentage':
+
+            # Configure the query
+            query = queries_file.return_all_region_by_tree_type_percentage(tree_types[args.reference_tree], args.min_percentage, args.max_percentage)
+            
+            ### TO DELETE --------------
+            query = query.replace(';','')
+            query = query + ' LIMIT 1000;'
+            # --------------------------
+
+            # Fetch data
+            data = fetch_from_label_db(query)
+
+            # Refromat data and split them by region
+            actual_region = None
+            actual_raster = None
+            for index, row in data.iterrows():
+                name = row['region_name']
+                if actual_region == None:
+                    features=[]
+                    actual_region = name
+                    actual_raster = row['raster_url']
+                else: 
+                    # Save intermediate data
+                    if actual_region != name:
+                        create_label_file(features, out_folder, actual_region)
+                        update_config(config, os.path.join(out_folder, actual_region), actual_raster)
+                        print('--created {} with {} points'.format(actual_region, len(features)))       
+                        print(actual_raster)
+                        actual_region = name
+                        actual_raster = row['raster_url']
+                        features=[]
+                tmp = json.loads(row['label_area'])
+                coordinates= [tmp['coordinates'][0][0][0], tmp['coordinates'][0][0][1], tmp['coordinates'][0][2][0],tmp['coordinates'][0][2][1]]
+                features.append(Feature(geometry=Point((row['feature_coordinate_x'],row['feature_coordinate_y']), precision=14), properties={"class": row['tree_type'], 
+                                        "bbox_id": row["label_id"]}, bbox=[coordinates[0],coordinates[2],coordinates[3],coordinates[1]])) # Be aware, the bbox id is not the same of the original label file
+
+                # Save final data
+                if index>= data.shape[0]-1:
+                    create_label_file(features, out_folder, actual_region)
+                    update_config(config, os.path.join(out_folder, actual_region), actual_raster)
+                    print('--created {} with {} points'.format(actual_region, len(features)))  
+                    print(actual_raster)
+
+
+        elif args.query_type == 'region_name':
+
+            # Configure the query
+            query = queries_file.return_all_labels_for_region(args.region)
+            
+            ### TO DELETE --------------
+            query = query.replace(';','')
+            query = query + ' LIMIT 1000;'
+            # --------------------------
+
+            # Fetch data
+            data = fetch_from_label_db(query)
+            
+            # Reformat data
+            features=[]
+            for index, row in data.iterrows():
+                tmp = json.loads(row['label_area'])
+                coordinates= [tmp['coordinates'][0][0][0], tmp['coordinates'][0][0][1], tmp['coordinates'][0][2][0],tmp['coordinates'][0][2][1]]
+                features.append(Feature(geometry=Point((row['feature_coordinate_x'],row['feature_coordinate_y']), precision=14), properties={"class": row['tree_type'], 
+                                        "bbox_id": row["label_id"]}, bbox=[coordinates[0],coordinates[2],coordinates[3],coordinates[1]])) # Be aware, the bbox id is not the same of the original label file
+            
+            # Save data
+            create_label_file(features, out_folder, args.region)
+            update_config(config, os.path.join(out_folder, args.region), row['raster_url'])
+            print('--created {} with {} points'.format(args.region, len(features)))  
+            print(row['raster_url'])
+
+
+        elif args.query_type == 'similar_season':
+
+            # Configure the query
+            query = queries_file.return_all_region_by_tree_type_percentage(tree_types[args.reference_tree], args.min_percentage, args.max_percentage)
+            
+            ### TO DELETE --------------
+            query = query.replace(';','')
+            query = query + ' LIMIT 1000;'
+            # --------------------------
+
+            # Fetch data
+            data = fetch_from_label_db(query)
+
+            # Refromat data and split them by region
+            actual_region = None
+            actual_raster = None
+            for index, row in data.iterrows():
+                name = row['region_name']
+                if actual_region == None:
+                    features=[]
+                    actual_region = name
+                    actual_raster = row['raster_url']
+                else: 
+                    # Save intermediate data
+                    if actual_region != name:
+                        create_label_file(features, out_folder, actual_region)
+                        update_config(config, os.path.join(out_folder, actual_region), actual_raster)
+                        print('--created {} with {} points'.format(actual_region, len(features)))       
+                        print(actual_raster)
+                        actual_region = name
+                        actual_raster = row['raster_url']
+                        features=[]
+                tmp = json.loads(row['label_area'])
+                coordinates= [tmp['coordinates'][0][0][0], tmp['coordinates'][0][0][1], tmp['coordinates'][0][2][0],tmp['coordinates'][0][2][1]]
+                features.append(Feature(geometry=Point((row['feature_coordinate_x'],row['feature_coordinate_y']), precision=14), properties={"class": row['tree_type'], 
+                                        "bbox_id": row["label_id"]}, bbox=[coordinates[0],coordinates[2],coordinates[3],coordinates[1]])) # Be aware, the bbox id is not the same of the original label file
+
+                # Save final data
+                if index>= data.shape[0]-1:
+                    create_label_file(features, out_folder, actual_region)
+                    update_config(config, os.path.join(out_folder, actual_region), actual_raster)
+                    print('--created {} with {} points'.format(actual_region, len(features)))  
+                    print(actual_raster)
+
+
+        else:
+            print('Invalid query type!')
+    except Exception as error:
+        print("Invalid input parameters!", error)
+        quit()
+    
+    print('Requested data was created')
+    
+    # Save configuration as YAML file
+    with open(os.path.join(out_folder, 'config.yaml'), 'w') as outfile:
+        yaml.dump(config, outfile, default_flow_style=False)
+
+
---
+++ postgres_cli/query_tools/utils/queries_file.py
@@ -0,0 +1,134 @@
+import datetime
+
+def return_tree_of_specific_type(tree_class: int):
+    '''
+        This query return Feature ID, Feature coordinates, Label ID, Session ID and Raster url
+    '''
+    query = "WITH FinalData(feature_id, feature_area, label_id, session_id) AS ( \
+            SELECT	F.id, ST_AsText(F.feature_area), L.id,	L.session_id \
+	        FROM public.label_feature F \
+            JOIN public.label L ON F.label_id = L.id \
+	        WHERE L.id = F.label_id AND F.class_id = {0}), \
+	        SessionRaster(session_id, raster_url) AS ( \
+            SELECT	S.id, R.url \
+            FROM public.label_session S \
+            JOIN public.raster_info R ON S.raster_info_id = R.id) \
+            SELECT  FD.feature_id, FD.feature_area, FD.label_id, SR.session_id, SR.raster_url \
+            FROM FinalData AS FD \
+            JOIN SessionRaster AS SR ON FD.session_id = SR.session_id ;".format(tree_class)
+    return query
+
+
+def return_all_region_for_a_tree_type(tree_class: int):
+    '''
+        This query return Feature ID, Feature tree class, Feature coordinates, Label ID, Session ID and Raster url
+    '''
+    query = "WITH LabelData(label_id, session_id) AS ( \
+            SELECT	L.id,L.session_id   \
+	        FROM public.label_feature F \
+		    JOIN public.label L ON F.label_id = L.id \
+	        WHERE L.id = F.label_id AND F.class_id = {}), \
+	        SessionRaster(session_id, raster_url) AS ( \
+            SELECT	S.id, R.url \
+	        FROM public.label_session S \
+            JOIN public.raster_info R ON S.raster_info_id = R.id)  \
+            SELECT  F.id, F.class_id AS tree_type, F.feature_area, LD.label_id,	SR.session_id,SR.raster_url \
+            FROM LabelData AS LD \
+	        JOIN SessionRaster AS SR ON LD.session_id = SR.session_id  \
+	        JOIN public.label_feature AS F ON F.label_id = LD.label_id ;".format(tree_class)
+    return query
+
+def return_all_region_by_tree_type_percentage(tree_class: str, min= 0, max= 100):
+    '''
+        Starting from a specific type of tree and its minimum and maximum presence (in percentage) return the labels with that conditions (and all the trees inside)
+        Feature ID, Feature coordinates, tree class, lable ID, Session ID, the raster url and the tree presence percentage
+    ''' 
+    query = "WITH SessionRaster(session_id, raster_url, area_name) AS ( \
+            SELECT	S.id, R.url, S.name\
+	        FROM public.label_session AS S JOIN public.raster_info R ON S.raster_info_id = R.id  \
+            WHERE S.classification = true)\
+	        , PartialCount(label_id, session_id, label_area ,counter) as ( \
+            SELECT	L.id, L.session_id, L.label_area, COUNT(*) \
+	        FROM public.label_feature AS F \
+		    JOIN public.label L ON F.label_id = L.id \
+            WHERE L.id = F.label_id AND F.class_id = {0}\
+            GROUP BY L.id) \
+            , TotalCount (label_id, counter) as (\
+            SELECT	L.id, COUNT(*) \
+            FROM public.label_feature F \
+            JOIN public.label L ON F.label_id = L.id \
+            WHERE L.id = F.label_id \
+            GROUP BY L.id ) \
+            , SelectedLabel (label_id, session_id, label_area, percentage) as ( \
+            SELECT	PC.label_id, PC.session_id, PC.label_area,(PC.counter::float/TC.counter::float)*100.0 \
+            FROM PartialCount AS PC \
+            JOIN TotalCount AS TC ON TC.label_id = PC.label_id \
+            JOIN SessionRaster AS SR ON SR.session_id = PC.session_id \
+            WHERE (PC.counter::float/TC.counter::float)*100.0 > {1} AND (PC.counter::float/TC.counter::float)*100.0 < {2}) \
+            SELECT  F.id AS feature_id, ST_x(F.feature_area) AS feature_coordinate_x ,ST_y(F.feature_area) AS feature_coordinate_y, TR.name AS tree_type, \
+                SL.label_id AS label_id, ST_AsGeoJSON(SL.label_area) AS label_area, SL.session_id AS session_id, SR.raster_url AS raster_url, SL.percentage AS percentage, SR.area_name AS region_name\
+            FROM SelectedLabel AS SL \
+            JOIN SessionRaster AS SR ON SR.session_id = SL.session_id \
+            JOIN public.label_feature AS F ON F.label_id = SL.label_id \
+            JOIN public.tree_class AS TR ON TR.id = F.class_id \
+            ORDER BY region_name".format(tree_class, min, max)
+            #ORDER BY label_id".format(tree_class, min, max)
+    return query
+
+
+def return_all_region_with_similar_period(starting_date: datetime.date.fromisoformat, days= 10):
+    '''
+        Starting from a specific type of tree and its minimum and maximum presence (in percentage) return the labels with that conditions (and all the trees inside)
+        Feature ID, Feature coordinates, tree class, lable ID, Session ID, the raster url and the tree presence percentage
+    ''' 
+    query = "WITH SessionRaster(session_id, raster_url, area_name) AS ( \
+            SELECT	S.id, R.url, S.name\
+	        FROM public.label_session AS S JOIN public.raster_info R ON S.raster_info_id = R.id  \
+            WHERE S.classification = true)\
+	        , PartialCount(label_id, session_id, label_area ,counter) as ( \
+            SELECT	L.id, L.session_id, L.label_area, COUNT(*) \
+	        FROM public.label_feature AS F \
+		    JOIN public.label L ON F.label_id = L.id \
+            WHERE L.id = F.label_id AND F.class_id = {0}\
+            GROUP BY L.id) \
+            , TotalCount (label_id, counter) as (\
+            SELECT	L.id, COUNT(*) \
+            FROM public.label_feature F \
+            JOIN public.label L ON F.label_id = L.id \
+            WHERE L.id = F.label_id \
+            GROUP BY L.id ) \
+            , SelectedLabel (label_id, session_id, label_area, percentage) as ( \
+            SELECT	PC.label_id, PC.session_id, PC.label_area,(PC.counter::float/TC.counter::float)*100.0 \
+            FROM PartialCount AS PC \
+            JOIN TotalCount AS TC ON TC.label_id = PC.label_id \
+            JOIN SessionRaster AS SR ON SR.session_id = PC.session_id \
+            WHERE (PC.counter::float/TC.counter::float)*100.0 > {1} AND (PC.counter::float/TC.counter::float)*100.0 < {2}) \
+            SELECT  F.id AS feature_id, ST_x(F.feature_area) AS feature_coordinate_x ,ST_y(F.feature_area) AS feature_coordinate_y, TR.name AS tree_type, \
+                SL.label_id AS label_id, ST_AsGeoJSON(SL.label_area) AS label_area, SL.session_id AS session_id, SR.raster_url AS raster_url, SL.percentage AS percentage, SR.area_name AS region_name\
+            FROM SelectedLabel AS SL \
+            JOIN SessionRaster AS SR ON SR.session_id = SL.session_id \
+            JOIN public.label_feature AS F ON F.label_id = SL.label_id \
+            JOIN public.tree_class AS TR ON TR.id = F.class_id \
+            ORDER BY region_name".format(datetime, days)
+            #ORDER BY label_id".format(tree_class, min, max)
+    return query
+
+
+def return_all_labels_for_region(region: str):
+    '''
+        Starting from the name of a region return all the avaiable labels for that region
+        Feature ID, Feature coordinates, tree class, lable ID, Session ID, the raster url and the tree presence percentage
+    ''' 
+    query = "SELECT F.id AS feature_id, ST_x(F.feature_area) AS feature_coordinate_x ,ST_y(F.feature_area) AS feature_coordinate_y, TC.name AS tree_type,\
+            L.id AS label_id, ST_AsGeoJSON(L.label_area) AS label_area, S.id AS session_id, R.url AS raster_url\
+            FROM public.label_session AS S  \
+            JOIN public.label AS L ON L.session_id = S.id  \
+            JOIN public.label_feature AS F ON F.label_id = L.id  \
+            JOIN public.tree_class AS TC ON TC.id = F.class_id  \
+            JOIN public.raster_info AS R ON R.id = S.raster_info_id  \
+            WHERE S.name= '{}' \
+            ORDER BY S.name;".format(region)
+    return query
+
+
+
